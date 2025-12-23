4176dd544a2c2368faedc93b69d9421ab9ce1165
Sajjad177 <sajjadhossainx06@gmail.com>
2025-12-24 01:33:44 +0600

    model fixed

--- src/modules/joinAsSupplier/joinAsSupplier.validation.ts
+++ src/modules/joinAsSupplier/joinAsSupplier.validation.ts
@@ -20,6 +20,9 @@ const joinAsSupplierValidation = z.object({
     email: z.string({
       required_error: "Email is required",
     }),
+    warehouseLocation: z.string({
+      required_error: "Warehouse location is required",
+    }),
     address: z.string({
       required_error: "Address is required",
     }),
