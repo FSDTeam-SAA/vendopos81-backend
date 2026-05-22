b34eb4fe3833bddf55c11086c4f302011c1cc8a2
Sajjad177 <sajjadhossainx06@gmail.com>
2026-05-22 16:38:21 +0600

    auth gurd added

--- src/modules/payment/payment.router.ts
+++ src/modules/payment/payment.router.ts
@@ -1,25 +1,20 @@
-import { Router } from "express";
-import auth from "../../middleware/auth";
-import { USER_ROLE } from "../user/user.constant";
-import paymentController from "./payment.controller";
+import { Router } from 'express';
+import auth from '../../middleware/auth';
+import { USER_ROLE } from '../user/user.constant';
+import paymentController from './payment.controller';
 
 const router = Router();
 
-router.post(
-  "/process",
-  auth(USER_ROLE.CUSTOMER),
-  paymentController.createPayment,
-);
-
-router.get("/get-all", paymentController.getAllPayments);
+router.post('/process', auth(USER_ROLE.CUSTOMER), paymentController.createPayment);
+router.get('/get-all', auth(USER_ROLE.ADMIN), paymentController.getAllPayments);
 
 router.post(
-  "/request-transfer/:id",
+  '/request-transfer/:id',
   auth(USER_ROLE.SUPPLIER),
   paymentController.requestForPaymentTransfer,
 );
 
-router.post("/transfer/:id", paymentController.transferPayment);
+router.post('/transfer/:id', auth(USER_ROLE.ADMIN), paymentController.transferPayment);
 
 const paymentRouter = router;
 export default paymentRouter;
